c29438625fc6b1deef0095d64ed8eb89c4c91b29
liupu <575866447@qq.com>
2019-05-31 23:29:40 +0800

    Movie getCharge

--- src/com/update/Movie.java
+++ src/com/update/Movie.java
@@ -30,4 +30,40 @@ public class Movie {
     public void setPriceCode(int priceCode) {
         this.priceCode = priceCode;
     }
+
+    /**
+     * 金额计算
+     */
+    public double getCharge(int daysRented) {
+        double result = 0;
+        // determine amounts for each line
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2) {
+                    result += (daysRented - 2) * 1.5;
+                }
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDRENS:
+                result += 1.5;
+                if (daysRented > 3) {
+                    result += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return result;
+    }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == NEW_RELEASE)
+                && daysRented > 1) {
+            return 2;
+        } else {
+            return 1;
+        }
+    }
 }
