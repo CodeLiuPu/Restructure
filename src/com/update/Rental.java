274aacae0d014793084c98ecb00b99f06cee79bd
liupu <575866447@qq.com>
2019-05-31 15:10:22 +0800

    v1

---
+++ src/com/update/Rental.java
@@ -0,0 +1,24 @@
+package com.update;
+
+/**
+ * @author : liupu
+ * date   : 2019/5/31
+ * desc   : 租赁
+ */
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+}
